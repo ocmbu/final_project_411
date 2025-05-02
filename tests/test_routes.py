b3c338b2c0eac5d99cad4f34f72d26a6aa212a58
Chamin Oh <ocm8403@bu.edu>
2025-05-01 23:41:55 -0400

    first test added

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -6,10 +6,18 @@ from app.models import User
 from app.models import Favorite
 from app.favorite_model import FavoriteLocationModel
 
-
 @pytest.fixture
-def sample_boxer1(session):
-    boxer = Boxers(name="Muhammad Ali", weight=210, height=191, reach=78, age=32)
-    session.add(boxer)
-    session.commit()
-    return boxer
+def Favlocation_model():
+    """ comment here """
+    return FavoriteLocationModel()
+
+def test_already_favorite(caplog):
+    """Test that adding already favorited location shoing error"""
+    model = FavoriteLocationModel()
+    model.add_favorite(1, "BU")
+    
+    with pytest.raises(ValueError) as exc_info:
+        model.add_favorite(1, "BU")
+
+    assert str(exc_info.value) == "BU is already favorited"
+    
