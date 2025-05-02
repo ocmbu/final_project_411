8759d9f68d97491639bb82540e413240b30f2e8a
Chamin Oh <ocm8403@bu.edu>
2025-05-01 23:20:45 -0400

    Changed imports

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -2,10 +2,14 @@ import time
 
 import pytest
 
-from app.models import model
-from app.routes import route
+from app.models import User
+from app.models import Favorite
+from app.favorite_model import FavoriteLocationModel
+
 
 @pytest.fixture
-def app_model():
-    
-    return ()
+def sample_boxer1(session):
+    boxer = Boxers(name="Muhammad Ali", weight=210, height=191, reach=78, age=32)
+    session.add(boxer)
+    session.commit()
+    return boxer
